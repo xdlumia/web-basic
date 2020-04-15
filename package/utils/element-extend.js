42758788c9dfb89ff161a3e3b86139a35a46cd80
晓冬 <xdq@live.cn>
2020-04-15 14:44:52 +0800

    基础包的内容按需引入

--- package/utils/element-extend.js
+++ package/utils/element-extend.js
@@ -2,15 +2,20 @@
  * @Author: web.闫超
  * @Date: 2018-09-14 18:34:55
  * @LastEditors: web.王晓冬
- * @LastEditTime: 2020-04-07 16:48:16
+ * @LastEditTime: 2020-04-15 14:43:26
  * @Description: elementUI扩展入口
  */
-import ElementUI from 'element-ui';
+import {
+  Input,
+  Image,
+  Dialog,
+  InputNumber
+} from 'element-ui';
 /**
  * 默认设置 el-input textarea 类型下的最大长度是300 text类型最大长度是32
  * maxlength是通过attrs传递给实际的Input,暂时找不到任何合适的地方去修改attrs，因此只能覆盖下render方法，在render之前修改下
  */
-ElementUI.Input.render = (function (old, defaultMaxLength) {
+Input.render = (function (old, defaultMaxLength) {
   return function () {
     if (this.$attrs.placeholder === undefined) {
       this.$attrs.placeholder = '请输入内容';
@@ -21,24 +26,27 @@ ElementUI.Input.render = (function (old, defaultMaxLength) {
 
     return old.apply(this, arguments);
   };
-}(ElementUI.Input.render, 200));
+}(Input.render, 200));
 
 // 修改element ui 默认参数
-ElementUI.Image.props.fit = { default: 'cover', type: String }
+Image.props.fit = {
+  default: 'cover',
+  type: String
+}
 
 /**
  * 修改默认值“close-on-click-modal”，默认情况下点击modal不关闭dialog
  */
-ElementUI.Dialog.props.closeOnClickModal.default = false;
-ElementUI.Dialog.props.closeOnPressEscape.default = false;
-ElementUI.Dialog.props.appendToBody.default = true;
-ElementUI.Dialog.props.modalAppendToBody.default = true;
-ElementUI.Dialog.props.top.default = '15px';
+Dialog.props.closeOnClickModal.default = false;
+Dialog.props.closeOnPressEscape.default = false;
+Dialog.props.appendToBody.default = true;
+Dialog.props.modalAppendToBody.default = true;
+Dialog.props.top.default = '15px';
 
 /**
  * InputNumber控制精度时，小数位全是0的时候，没必要强制显示小数位
  */
-ElementUI.InputNumber.computed.currentInputValue = function currentInputValue() {
+InputNumber.computed.currentInputValue = function currentInputValue() {
   const currentValue = this.currentValue;
   if (typeof currentValue === 'number' && this.precision !== undefined) {
     return currentValue.toFixed(this.precision);
@@ -46,5 +54,3 @@ ElementUI.InputNumber.computed.currentInputValue = function currentInputValue() 
     return currentValue;
   }
 };
-
-
