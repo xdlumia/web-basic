1513a71776794c69c92aaba93db67cf1f73f782f
晓冬 <xdq@live.cn>
2020-04-15 14:39:55 +0800

    修改api服务

--- examples/api/service-api.js
+++ examples/api/service-api.js
@@ -2,69 +2,92 @@
  * @Author: web.王晓冬
  * @Date: 2020-03-20 18:25:34
  * @LastEditors: web.王晓冬
- * @LastEditTime: 2020-04-14 14:43:46
+ * @LastEditTime: 2020-04-15 14:39:02
  * @Description: file content
  */
-const MOCK_ADDRESS = '/mock/5de4c965e8c6606de89f330a/operlogService'
+const MOCK_ADDRESS = ''
 
 export default {
   __mockAddress: MOCK_ADDRESS,
-  /* =============操作日志start=========== */
-  // 查看详细信息
-  logInfo: {
-    'url': '/log/info/{id}',
+  /* =============人机结合API=========== */
+  // 可选客服人员
+  projectUserUsers: {
+    'url': 'ProjectUser/users',
+    'methods': 'post',
+    'mock': false,
+    'showMsg': false
+  },
+  // 已选客服人员
+  projectGroupDetail: {
+    'url': '/ProjectGroup/detail/{groupId}',
+    'mock': false,
+  },
+  // 新增修改项目组
+  projectGroupAddOrUpdate: {
+    'url': '/ProjectGroup/addOrUpdate',
+    'methods': 'post',
+    'mock': false,
+  },
+  // 删除项目组
+  projectGroupDel: {
+    'url': '/ProjectGroup/del/{groupId}',
+    'methods': 'del',
+    'mock': false,
+  },
+  // 项目组分页条件查询
+  projectGroupPage: {
+    'url': '/ProjectGroup/page',
+    'methods': 'post',
     'mock': false,
     'showMsg': false
   },
 
-  // 获取列表
-  logList: {
-    'url': '/log/list',
+
+  // 新增修改规则
+  aiRuleAddOrUpdate: {
+    'url': '/AiRule/addOrUpdate',
     'mock': false,
-    'showMsg': false
+    'methods': 'post'
   },
 
-  // 业务系统查询日志的接口
-  logQueryList: {
-    'url': '/log/queryList',
+  // 规则详情
+  aiRuleDetail: {
+    'url': '/AiRule/detail/{ruleId}',
     'mock': false,
-    'showMsg': false
+    'methods': 'get'
   },
 
-  // 保存
-  logSave: {
-    'url': '/log/save',
+  // 规则删除
+  aiRuleDel: {
+    'url': '/AiRule/del/{ruleId}',
+    'mock': false,
+    'methods': 'del'
+  },
+
+  // 规则分页条件查询
+  aiRulePage: {
+    'url': '/AiRule/page',
     'mock': false,
     'methods': 'post',
     'showMsg': false
   },
-
-  // 批量保存
-  logSaveBatchLogs: {
-    'url': '/log/saveBatchLogs',
+  // 规则列表上下移动接口
+  aiRuleUpdateRank: {
+    'url': '/AiRule/updateRank',
     'mock': false,
-    'showMsg': false,
+    'methods': 'post'
+  },
+  // 规则列表上下移动接口
+  aiDepartmentList: {
+    'url': '/AiDepartment/list',
+    'mock': false,
+  },
+
+  // 登陆验证
+  validUser: {
+    'url': '/ValidUser',
+    'mock': false,
     'methods': 'post'
   },
 
-  // 更新平台日志
-  logUpdatePlatformLog: {
-    'url': '/log/updatePlatformLog',
-    'mock': false,
-    'showMsg': false,
-    'methods': 'post'
-  },
-  /* =============操作日志end=========== */
-
-  /* =============RocketMQ失败重试start=========== */
-  // 重试失败消息
-  commit: {
-    'url': 'http://git.kinfe.net/api/v4/projects/21/repository/commits?per_page=100&page=1&private_token=Wz6K47F6HKPp1RUYTKxd&ref=dev',
-    'mock': false,
-    'showMsg': true,
-    'methods': 'get'
-  },
-  /* =============RocketMQ失败重试end=========== */
-
-  /* =============文件压缩下载（日志和配置文件）end=========== */
 }
