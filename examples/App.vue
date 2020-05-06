eb95d61984a68d83f802798d20fbf910a23df53d
晓冬 <xdq@live.cn>
2020-05-06 16:33:54 +0800

    修改基础

--- examples/App.vue
+++ examples/App.vue
@@ -2,30 +2,28 @@
  * @Author: web.王晓冬
  * @Date: 2020-03-19 10:29:17
  * @LastEditors: web.王晓冬
- * @LastEditTime: 2020-03-25 10:24:59
+ * @LastEditTime: 2020-05-06 16:27:42
  * @Description: file content
  */
 <template>
   <div id="app">
-     <hr/>
-     <h1 class="ac">fsfsdfsf</h1>
-    <HelloWorld msg="1584945328"/>
+    <hr />
+    <h1 class="ac mt50">fsfsdfsf</h1>
+    <HelloWorld msg="1584945328" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-import vue from "vue"
+import HelloWorld from "./components/HelloWorld.vue";
+import vue from "vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld,
+    HelloWorld
   },
-  created(){
-  }
+  created() {}
 };
 </script>
 
 <style>
-
 </style>
