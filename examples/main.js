5218b2db8ff09780e0bd62c0d4f02132e89f9229
晓冬 <xdq@live.cn>
2020-06-02 09:36:52 +0800

    修改示例文件

--- examples/main.js
+++ examples/main.js
@@ -2,7 +2,7 @@
  * @Author: web.王晓冬
  * @Date: 2020-03-19 10:29:17
  * @LastEditors: web.王晓冬
- * @LastEditTime: 2020-05-27 17:03:05
+ * @LastEditTime: 2020-06-01 17:40:02
  * @Description: file content
  */
 import Vue from 'vue';
@@ -14,18 +14,14 @@ import {
   eWebBasic
 } from '../package/index';
 // import { eWebBasic } from '../lib/e-web-basic.umd';
+console.log(ElementUI.Dialog.props.top);
+
 
 import './api'
 
 Vue.use(ElementUI, {
   size: 'mini'
 });
-
-import Schema from 'async-validator';
-// 修改form必填项验证的默认提示
-Schema.messages.required = () => '此处是必填项'
-// 输入框默认显示字数
-ElementUI.Input.props.showWordLimit.default = true
 Vue.use(eWebBasic);
 new Vue({
   router,
