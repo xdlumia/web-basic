4677b6823b7ddeccd3864e0e8f74d92ac56189b4
xdlumia <xdq@live.cn>
2020-11-03 19:04:14 +0800

    增加v-player组件

---
+++ package/components/icon/index.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: web.王晓冬
+ * @Date: 2020-10-29 10:08:49
+ * @LastEditors: web.王晓冬
+ * @LastEditTime: 2020-11-03 17:22:43
+ * @Description: Table
+ */
+import Icon from './main';
+
+/* istanbul ignore next */
+Icon.install = function(Vue) {
+  Vue.component(Icon.name, Icon);
+};
+
+export default Icon;
---
+++ package/components/icon/main.vue
@@ -0,0 +1,44 @@
+/*
+ * @Author: web.王晓冬
+ * @Date: 2020-10-27 10:31:35
+ * @LastEditors: web.王晓冬
+ * @LastEditTime: 2020-11-03 17:23:21
+ * @Description: file content
+*/
+<template>
+  <svg class="d-icon" :style="iconSize" @click="handleClick" aria-hidden="true">
+    <use :xlink:href="`#${icon}`" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "d-icon",
+
+  props: {
+    icon: String,
+    size: [Number, String],
+  },
+  methods: {
+    handleClick(evt) {
+      this.$emit("click", evt);
+    },
+  },
+  computed: {
+    iconSize() {
+      let size = /^\d+$/.test(this.size) ? this.size + "px" : this.size;
+      return { width: size, height: size };
+    },
+  },
+};
+</script>
+<style lang="less" scoped>
+.d-icon {
+  cursor: pointer;
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
